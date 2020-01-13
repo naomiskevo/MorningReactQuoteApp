30c5d933860bbe4d9789f19083c2b0713453c0e3
naomiskevo <naomiskevo@gmsail.com>
2020-01-13 10:48:15 -0600

    initla commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
+import Button from './components/Button'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  state = {
+    isTrue: false,
+    isToggled: false,
+  }
+
+  handleSubmit = () => {
+    this.setState({
+      isTrue : !this.state.isTrue
+    })
+  }
+
+  handleToggle = () => {
+    this.setState({
+      isToggled : !this.state.isToggled
+    })
+  }
+
+
+  render() {
+    return (
+      <div className="App" >
+        <header className="App-header">
+          {this.state.isToggled ? null : (
+            <img src={logo} className="App-logo" alt="logo" />
+          )}
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <h1>Party</h1>
+          <Button handleClick={this.handleToggle} label="Toggle"/>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        <Button handleClick={this.handleSubmit} label={this.state.isTrue ? "Done" : "submit"}/>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+
